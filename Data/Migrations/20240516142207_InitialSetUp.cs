9a1d495eff30c2b0b4bb0c823f1663da6934fab1
TasniaKheya <23kheya@gmail.com>
2024-05-16 21:34:36 +0600

    ASP.NET Core with ORM Database

---
+++ Data/Migrations/20240516142207_InitialSetUp.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JokesWebApp.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialSetUp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Jokes",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    JokeQuestion = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    JokeAnswer = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Jokes", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Jokes");
+        }
+    }
+}
---
+++ Models/Jokes.cs
@@ -0,0 +1,9 @@
+﻿namespace JokesWebApp.Models
+{
+    public class Jokes
+    {
+        public int id { get; set; }
+        public string JokeQuestion { get; set; }
+        public string JokeAnswer { get; set; }
+    }
+}
